5818f39f5a7c17c1d212e5493e74f2d5309f4a6c
khorshed-alam-kbd <khorshedalam.kbd@gmail.com>
2024-08-18 14:41:01 +0600

    update

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Form } from 'react-router-dom';
-import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from 'react-icons/fa';
+import React from "react";
+import { Form } from "react-router-dom";
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
-import bannerImg from '../../Assets/photos/contact-img.jpg'
-import ContactForm from '../../components/ContactForm';
+import bannerImg from "../../Assets/photos/contact-img.jpg";
+import ContactForm from "../../components/ContactForm";
 
 const ContactUs = () => {
-    return (
-        <div id='contact'>
-            {/* <div className="hero py-8" style={{ backgroundImage: `url(${bannerImg})` }}>
+  return (
+    <div id="contact">
+      {/* <div className="hero py-8" style={{ backgroundImage: `url(${bannerImg})` }}>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
@@ -17,27 +17,38 @@ const ContactUs = () => {
                     </div>
                 </div>
             </div> */}
-            {/* contact information  */}
-            <div id='contact'>
-                <div className='mx-auto px-10 bg-base-100 my-10 max-w-7xl'>
-                    <div className='text-center my-8 ' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000">
-                        <h1 className="text-4xl font-bold mb-2">LET'S TALK</h1>
-                        <hr className='w-24 h-1 mx-auto text-xl bg-orange-400 rounded' />
-                        <p className="pt-2 max-w-4xl mx-auto text-gray-600 text-justify lg:text-center">Let's Get in Touch ! Have a Query ? Send Us a Message</p>
-                    </div>
-                    <ContactForm></ContactForm>
-
-                </div>
-            </div>
-
-            {/* Google map location */}
-            <div className='mx-auto w-full'>
-                <iframe className='mx-auto w-full' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7304.909797720126!2d90.41131330915418!3d23.731152444398056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9962f3cdbef%3A0xe81c436da5772a41!2sA%20Jharna%20Trade%20International!5e0!3m2!1sen!2sbd!4v1690404494225!5m2!1sen!2sbd" height="350" alt='google map' loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-            </div>
-
+      {/* contact information  */}
+      <div id="contact">
+        <div className="mx-auto px-10 bg-base-100 my-10 max-w-6xl">
+          <div
+            className="text-center my-8 "
+            data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom"
+            data-aos-duration="1000"
+          >
+            <h1 className="text-4xl font-bold mb-2">LET'S TALK</h1>
+            <hr className="w-24 h-1 mx-auto text-xl bg-orange-400 rounded" />
+            <p className="pt-2 max-w-4xl mx-auto text-gray-600 text-justify lg:text-center">
+              Let's Get in Touch ! Have a Query ? Send Us a Message
+            </p>
+          </div>
+          <ContactForm></ContactForm>
         </div>
-    );
+      </div>
+
+      {/* Google map location */}
+      <div className="mx-auto w-full">
+        <iframe
+          className="mx-auto w-full"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7304.909797720126!2d90.41131330915418!3d23.731152444398056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9962f3cdbef%3A0xe81c436da5772a41!2sA%20Jharna%20Trade%20International!5e0!3m2!1sen!2sbd!4v1690404494225!5m2!1sen!2sbd"
+          height="350"
+          alt="google map"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,84 +1,159 @@
-import React, { useState } from 'react';
-import { FaEnvelope, FaFacebook, FaHome, FaList, FaPhoneAlt, FaTimes } from 'react-icons/fa';
-import img from '../../../Assets/photos/logo.png'
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  FaEnvelope,
+  FaFacebook,
+  FaHome,
+  FaList,
+  FaPhoneAlt,
+  FaTimes,
+} from "react-icons/fa";
+import img from "../../../Assets/photos/logo.png";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-    const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const handleButtonClick = () => {
+    setIsButtonClicked(!isButtonClicked);
+  };
 
-    const handleButtonClick = () => {
-        setIsButtonClicked(!isButtonClicked);
-    };
+  const { pathname } = useLocation();
 
-    const { pathname } = useLocation();
+  return (
+    <>
+      <header className="py-3 bg-blue-950 text-white hidden lg:block">
+        <div className="mx-auto max-w-6xl px-8 lg:px-0  flex justify-between items-center">
+          <div className="flex items-center gap-2 my-1">
+            <FaEnvelope />
+            <p>comtechsbd@gmail.com</p>
+          </div>
 
-    return (
-        <>
-            <header className='py-3 bg-slate-800 text-white hidden lg:block' >
-                <div className='mx-auto max-w-6xl px-8 lg:px-0  flex justify-between items-center'>
-                    <div className='flex items-center gap-2 my-1'  >
-                        <FaEnvelope />
-                        <p>comtechsbd@gmail.com</p>
-                    </div>
+          <div className="flex items-center gap-3 my-1">
+            <div className="flex items-center gap-2">
+              <FaPhoneAlt />
+              <p className="text-md">+8801620658621</p>
+            </div>
+          </div>
+        </div>
+      </header>
 
-                    <img className='h-10 lg:rounded hidden block ' src={img} alt="" />
+      <nav className="bg-white ">
+        <div className="navbar mx-auto  max-w-6xl ">
+          <img className="h-10 hidden lg:flex " src={img} alt="" />
 
-                    <div className='flex items-center gap-3 my-1'>
-                        <div className='flex items-center gap-2'  >
-                            <FaPhoneAlt />
-                            <p className='text-md'>+8801620658621</p>
-                        </div>
-                    </div>
-                </div >
-            </header >
-            < nav className='bg-blue-950 ' >
-                <div className="navbar  mx-auto  max-w-6xl ">
-                    <img className='h-10 rounded hidden lg:flex ' src={img} alt="" />
+          <div className=" navbar-center hidden lg:flex mx-auto">
+            <ul className="flex items-center text-blue-950 font-semibold gap-3">
+              <li>
+                {" "}
+                <Link
+                  to="/"
+                  className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${
+                    pathname === "/" ? "border-b-2 border-orange-500" : ""
+                  }`}
+                >
+                  HOME
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  to="/services"
+                  className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${
+                    pathname === "/services"
+                      ? "border-b-2 border-orange-500"
+                      : ""
+                  }`}
+                >
+                  SERVICES
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  to="/about-us"
+                  className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${
+                    pathname === "/about-us"
+                      ? "border-b-2 border-orange-500"
+                      : ""
+                  }`}
+                >
+                  ABOUT US
+                </Link>
+              </li>
+              {/* < li > <Link to='/training' className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${pathname === '/training' ? "border-b-2 border-orange-500" : ""}`}>TRANING</Link></li> */}
+              <li>
+                {" "}
+                <Link
+                  to="/contact-us"
+                  className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${
+                    pathname === "/contact-us"
+                      ? "border-b-2 border-orange-500"
+                      : ""
+                  }`}
+                >
+                  CONTACT US
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-                    <div className=" navbar-center hidden lg:flex mx-auto">
-                        <ul className="flex items-center text-white font-semibold gap-3">
-                            < li > <Link to='/' className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${pathname === '/' ? "border-b-2 border-orange-500" : ""}`}>HOME</Link></li>
-                            < li > <Link to='/services' className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${pathname === '/services' ? "border-b-2 border-orange-500" : ""}`}>SERVICES</Link></li>
-                            < li > <Link to='/about-us' className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${pathname === '/about-us' ? "border-b-2 border-orange-500" : ""}`}>ABOUT US</Link></li>
-                            {/* < li > <Link to='/training' className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${pathname === '/training' ? "border-b-2 border-orange-500" : ""}`}>TRANING</Link></li> */}
-                            < li > <Link to='/contact-us' className={`hover:border-b-2  hover:border-orange-500 pb-2 px-4 mx-2 ${pathname === '/contact-us' ? "border-b-2 border-orange-500" : ""}`}>CONTACT US</Link></li>
-                        </ul>
-                    </div>
+          <div className="px-3 w-full lg:w-0 flex item-center justify-between">
+            <div
+              className="lg:hidden text-blue-950 text-xl "
+              onClick={handleButtonClick}
+            >
+              {isButtonClicked ? <FaTimes /> : <FaList />}
+            </div>
 
+            <div className={!isButtonClicked ? "lg:hidden" : "hidden"}>
+              <img className="h-10" src={img} alt="" />
+            </div>
 
-                    <div className='px-3 w-full lg:w-0'>
-                        <div className="lg:hidden text-white text-xl " onClick={handleButtonClick}>
-                            {
-                                isButtonClicked ? <FaTimes /> : <FaList />
-                            }
-                        </div>
+            <div className={isButtonClicked ? "lg:hidden w-[400px]" : "hidden"}>
+              <ul className="text-blue-950 font-semibold gap-3 ">
+                <li>
+                  {" "}
+                  <Link to="/" className="pb-2 px-4 mb-2">
+                    HOME
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/services" className="pb-2 px-4 mb-2">
+                    SERVICES
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/about-us" className="pb-2 px-4 mb-2">
+                    ABOUT US
+                  </Link>
+                </li>
+                {/* < li > <Link to='/training' className='pb-2 px-4 mb-2'>TRANING</Link></li> */}
+                <li>
+                  {" "}
+                  <Link to="/contact-us" className="pb-2 px-4 mb-2">
+                    CONTACT US
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-                        <div className={isButtonClicked ? 'lg:hidden w-[400px]' : 'hidden'}>
-                            <ul className="text-white font-semibold gap-3 ">
-                                < li > <Link to='/' className='pb-2 px-4 mb-2'>HOME</Link></li>
-                                < li > <Link to='/services' className='pb-2 px-4 mb-2'>SERVICES</Link></li>
-                                < li > <Link to='/about-us' className='pb-2 px-4 mb-2'>ABOUT US</Link></li>
-                                {/* < li > <Link to='/training' className='pb-2 px-4 mb-2'>TRANING</Link></li> */}
-                                < li > <Link to='/contact-us' className='pb-2 px-4 mb-2'>CONTACT US</Link></li>
-
-                            </ul>
-                        </div>
-
-                        <div className='navbar lg:hidden flex justify-end' >
-                            <div className='flex items-center text-white font-semibold gap-3  '>
-                                <a href="tel:+8801620658621"><FaPhoneAlt /></a>
-                                <a href="mailto:>comtechsbd@gmail.com"><FaEnvelope /></a>
-
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-            </nav >
-        </>
-    );
+            <div className="lg:hidden">
+              <div className="flex items-center text-blue-950 font-semibold gap-3  ">
+                <a href="tel:+8801620658621">
+                  <FaPhoneAlt />
+                </a>
+                <a href="mailto:>comtechsbd@gmail.com">
+                  <FaEnvelope />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
